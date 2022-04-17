7ed1211e391968b8ffe6c45453cda740dc20a03f
MattCornicDev <glgharib@gmail.com>
2022-04-18 01:20:44 +0200

    Routes et pages

---
+++ src/pages/About.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation/>
+            <h1>A propos</h1>
+            <br/>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, eum?</p>
+            <br/>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad adipisci blanditiis consectetur cupiditate
+                dolores expedita facilis, incidunt laborum libero magni minus, placeat provident quae quo reiciendis!
+                Minima minus numquam quas? Cupiditate, distinctio doloremque, error fugit nesciunt nisi provident
+                reprehenderit sed tempore unde velit vero voluptatibus? Aliquid, amet illum ipsum neque numquam officiis
+                qui quos sunt. Alias amet architecto cumque dicta distinctio dolor eaque est excepturi explicabo in
+                itaque, iusto laboriosam modi natus odio optio placeat quaerat quasi repellat repudiandae rerum
+                similique suscipit tempora temporibus totam! Animi aperiam, autem consectetur eaque eius libero placeat,
+                quam quisquam quos recusandae totam voluptate. Aperiam.</p>
+        </div>
+    );
+};
+
+export default About;
